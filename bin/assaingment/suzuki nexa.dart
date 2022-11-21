c2498e53358434fb83579558452fd78bb7729813
SHIBILISBK <shibilimohammed006@gmail.com>
2022-11-21 20:36:35 +0530

    commit

---
+++ bin/assaingment/suzuki nexa.dart
@@ -0,0 +1,53 @@
+class Nexa{
+
+
+  String? name = "";
+  int prize = 1045000;
+  int model = 2022;
+  String color = "blue";
+  int varient = 4;
+  int enginecapacity =  1462 ;
+  int seatcapacity = 5;
+  int fuelcapacity = 45;
+  static String company = "suzuki nexa";
+
+}
+
+void main(){
+  Nexa c1 = Nexa();
+      print("car 1 Nexa");
+  print('Name = ${c1.name = "GRAND VITARA"}');
+  print('Prize = ${c1.prize}');
+  print('Model = ${c1.model}');
+  print('Colr = ${c1.color}');
+  print('Varient = ${c1.varient}');
+  print('Enginecapacity = ${c1.enginecapacity}');
+  print('Seatcapacity = ${c1.seatcapacity}');
+  print('Fuelcapacity = ${c1.fuelcapacity}');
+  
+
+  Nexa c2 = Nexa();
+  print("car 2 Nexa");
+  print('Name = ${c2.name = "XL6"}');
+  print('Prize = ${c2.prize = 1129000}');
+  print('Model = ${c2.model}');
+  print('Color = ${c2.color =" red"}');
+  print('Varient = ${c2.varient}');
+  print('Enginecapacity = ${c2.enginecapacity}');
+  print('Seatcapacity = ${c2.seatcapacity = 6}');
+  print('Fuelcapacity = ${c2.fuelcapacity}');
+  
+  
+  Nexa c3 = Nexa();
+  print("car 3 Nexa");
+  print('Name = ${c3.name = "BALENO"}');
+  print('Prize = ${c3.prize = 649000}');
+  print('Model = ${c3.model}');
+  print('Color = ${c3.color = "grey"}');
+  print('Varient = ${c3.varient}');
+  print('Enginecapacity = ${c3.enginecapacity = 1197}');
+  print('Seatcapacity ${c3.seatcapacity = 5}');
+  print('Fuelcapacity = ${c3.fuelcapacity = 37}');
+
+
+}
