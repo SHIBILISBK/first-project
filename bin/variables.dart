16fa0b560cc8ef4cf19d38ebbba2a3d8362fb8ff
SHIBILISBK <shibilimohammed006@gmail.com>
2022-11-21 15:14:36 +0530

    commit

---
+++ bin/variables.dart
@@ -0,0 +1,15 @@
+void main(){
+  String name ="anu";
+  int age = 20;
+  int phone = 8868787273;
+  double mark = 8.3;
+  String email ="anu006@gmail.com";
+
+  print("--my resume--");
+  print("my name is ${name}"); //string interpolation = $(variable name) my name is name
+  print("i am ${age} yrs oid");
+  print("my phone number is ${phone}");
+  print("i have ${mark} in my graduation");
+  print("my email id is ${email}");
+
+}
